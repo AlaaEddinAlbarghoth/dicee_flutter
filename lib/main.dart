242da8c7fe6bc255c409f5a11310f4fec2866419
Alaa Eddin Albarghoth <alaaeddin.albarghoth@appcent.mobi>
2022-05-17 11:45:52 +0300

    Build the body

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,42 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
+/* State less widget will refresh itself every time you hit save */
 class DicePage extends StatelessWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var leftDicNo = 4;
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('left');
+              },
+              child: Image.asset(
+                  'images/dice$leftDicNo.png'), // String interpolation
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('right');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
